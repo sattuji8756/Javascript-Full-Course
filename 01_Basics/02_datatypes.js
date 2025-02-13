e51dbdb6e2ce05e80a8ef11855441531cb1a2edd
Satyendra kumar <140686428+sattuji8756@users.noreply.github.com>
2025-02-13 08:39:05 +0000

    ECMA and datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"//trate all js code as a newer version but now this it is not mandatory beacause automatically run in standard version
+
+// alert(3+3) // we are using node not brower
+
+// console.log(3+3); console.log("this is wrong beacause collon is not given")//but after given colon readibitliy is very poor so this consider wrong practice
+
+// Hitesh sir code is started here
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
