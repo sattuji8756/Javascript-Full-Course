e2827f3d6ccb67ea25fb62392856e924c7fec58e
Satyendra kumar <140686428+sattuji8756@users.noreply.github.com>
2025-02-15 04:36:58 +0000

    Comparison Of Number & String

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,35 @@
+////////////////////Lecture 08//////////////////////////////////////////
+
+// console.warn(2>1)
+// console.warn(2>=1)
+// console.warn(2<1)
+// console.warn(2<=1)
+// console.warn(2==1)
+// console.warn(2!=1)
+// console.warn(2===1)
+
+
+// // typescript did not allow to compare different datatype 
+// console.log("2">1)
+// console.log("02">1)
+
+console.log(null>0);  //False
+console.log(null<0);  //False
+console.log(null==0);  //False
+console.log(null>=0);  //true
+console.log(null<=0);  //true
+
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+console.log(undefined==0);
+
+// strict check 
+
+console.log("2"==2);
+console.log("2"===2);
+console.log("2"==="2");
+
+
