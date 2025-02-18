23c516d2eaed3b1a4dcc6a6add84cacbd2fff27a
Satyendra kumar <140686428+sattuji8756@users.noreply.github.com>
2025-02-18 08:38:39 +0000

    Controle flow and code file

---
+++ 04_Control_flow/02_switch.js
@@ -0,0 +1,36 @@
+/////////////////////////LECTURE 26 CONTROL FLOW/////////////////////////
+
+// SYNTAX SWITCH 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// TO COPY ALL CODE Use
+// SELECT + SHIFT + ALT + DOWN ARROW 
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
