341c9d1617dba027a5e114b420dc7a9badc4b6ff
Satyendra kumar <140686428+sattuji8756@users.noreply.github.com>
2025-02-21 07:21:29 +0000

    using reduce method in loops

---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////LECTURE 30////////////////////////////////////////
+// ye reduce method specially shopping wagera main kaam aata hai 
+
+
+const myNums = [1, 2, 3]
+
+// function 
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// arrow function 
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
