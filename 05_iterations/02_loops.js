59e38b88ee510703c67626bcd2ab47578bef5596
Satyendra kumar <140686428+sattuji8756@users.noreply.github.com>
2025-02-21 07:03:37 +0000

    while and do while loops

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,29 @@
+////////////////////////LECTURE 28///////////////////////////////////////////////
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+//////////////////////////////////////////////////////////
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+////////////////////////////////////////////////////
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// programming main kuch galat sahi nahi hai end of the day jo aapko bola gaya hai hai bas vo result leke aana hai fir chahe aap for loop ya while loop se lao 
